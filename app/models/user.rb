43f648ca85a06707efe6dc1c0ecce41c6fb14f80
imran5644 <imi56444@gmail.com>
2020-12-19 02:50:00 +0530

    implements the user model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :username, presence: { message: "must be given please" }
+    validates :username, length: { minimum: 5 }
+    validates :username, uniqueness: {scope: :username, message: "This username is taken!"}
+    validates :username, format: { with: /\A[0-9A-Za-z]+\z/,  message: 'alphanumeric only!' }
+end
