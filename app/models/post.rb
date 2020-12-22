edda06e4bbc62a1659e65e9fbfdb0138f8067b2c
cgcarlosg <cgcarlosg@hotmail.com>
2020-12-22 10:01:09 -0500

    changes according to the code reviewer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
   validates :title, uniqueness: true
+  validates :title, :body, length: { minimum: 3}
   belongs_to :user, class_name: :User, foreign_key: :user_id
   has_many :comments
 end
